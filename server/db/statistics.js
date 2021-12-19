05ee373dba8f8396b4f7a8ce63c7d7d7428e09fb
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2021-12-19 01:38:17 -0500

    index added to models

--- server/db/statistics.js
+++ server/db/statistics.js
@@ -4,13 +4,23 @@ const mongoose = require('mongoose')
 const Statistic = mongoose.model(
   "Statistic",
   new mongoose.Schema({
-    guess: Number,
+    guess: { type: Number, index: true },
     count: Number,
     interval: {
       type: mongoose.Schema.Types.ObjectId,
+      index: true,
       ref: "Interval"
     }
   })
 );
 
+Statistic.findByIntervalId = async function(intervalId) {
+    const stats = await this.find({interval: intervalId});
+    return stats;
+}
+
+Statistic.findByGuess = async function(intervalId, guess) {
+  const stat = await this.findOne({interval: intervalId, guess: guess});
+  return stat;
+}
 module.exports = Statistic;
